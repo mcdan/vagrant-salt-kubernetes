6a420500556c2d6ab85b51bf742a461f56ab0034
dan mcweeney <mcweeney@adobe.com>
2017-11-30 15:59:06 -0500

    Extract admin config

---
+++ getAdminConfig.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+port=$(vagrant ssh-config master | grep Port | tr -s " " | cut -d ' ' -f3)
+idfile=$(vagrant ssh-config master | grep IdentityFile | tr -s " " | cut -d ' ' -f3)
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P ${port} -i ${idfile} ubuntu@127.0.0.1:/opt/k8s/conf/admin.kubeconfig .
