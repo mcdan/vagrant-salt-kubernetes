8e27181e2e02ff6ada66aec459de4134cd034598
dan mcweeney <mcweeney@adobe.com>
2017-12-05 10:23:37 -0500

    Fix up all

--- up-all.sh
+++ up-all.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-vagrant up master &;vagrant up node-1 &; vagrant up node-2 &;vagrant up node-3 &;vagrant up node-4 &
+vagrant up master &
+vagrant up node-1 &
+vagrant up node-2 &
+vagrant up node-3 &
+vagrant up node-4 &
