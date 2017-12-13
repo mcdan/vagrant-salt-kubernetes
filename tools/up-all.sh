2260c27be6b797050364a080f8085d0f7a021588
dan mcweeney <mcweeney@adobe.com>
2017-12-13 14:25:21 -0500

    Improve up script

--- tools/up-all.sh
+++ tools/up-all.sh
@@ -10,22 +10,11 @@ if [[ -z "${NUM_NODES=}" ]]; then
 fi
 
 vagrant up master
-PIDS_TO_WAIT_ON=()
 for ((i=1; i <= NUM_NODES ; i++)); do
   vagrant up node-${i} &
-  PIDS_TO_WAIT_ON+=($!)
 done
 
-PID_DONE=0
-while [ ${PID_DONE} -lt ${#PIDS_TO_WAIT_ON[@]} ];do
-  for pid in "${PIDS_TO_WAIT_ON[@]}"; do
-    ps | grep ${pid} > /dev/null
-    if [ $? -ne 0 ]; then
-      PID_DONE=$((PID_DONE + 1))
-    fi
-  done
-  printf "Waiting for vagrant to finish...\n"
-  sleep 10s
-done
+
+wait
 printf "Vagrant Done, bootstrapping gluster cluster\n"
 ${DIR}/heketi-configure.sh
